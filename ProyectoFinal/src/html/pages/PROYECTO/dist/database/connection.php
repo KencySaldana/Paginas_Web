9bc6fd8a2f2a4aba990f126bd56b74f9e72b6627
KencySaldana <2030024@upv.edu.mx>
2023-04-13 23:23:40 -0600

    se agrega categorias

--- ProyectoFinal/src/html/pages/PROYECTO/dist/database/connection.php
+++ ProyectoFinal/src/html/pages/PROYECTO/dist/database/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$dsn = 'mysql:dbname=systemKY;host=157.245.140.190;';
+	$dsn = 'mysql:dbname=systemKY;host=localhost;';
 	$user = 'admin';
 	$password = '52f9d1ac770d01b9be9df4c1d5edfd3495784ea1c5e03532';
 
